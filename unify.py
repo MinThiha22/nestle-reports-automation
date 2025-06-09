d67441d128553bbccbcd7e27ed2d52400ea53ca9
Min Thiha Ko Ko (Mike) <minthihakoko@gmail.com>
2025-06-10 00:48:42 +1200

    Export all flat files tab to excel

--- unify.py
+++ unify.py
@@ -37,31 +37,51 @@ def login_unify(p):
 
 def automate_download(page):
   try:
+    # Navigate to Favourite, Choose Flat files 2
     locate_and_action(page, '#FavoritesLink', description="Favourite")
     locate_and_action(page, 'span', has_text="Flat Files 2", description="Flat Files 2")
     locate_and_action(page, 'div.thumb-box', has_text="Flat File - CD", description="CD Card")
     time.sleep(2)
     
-    locate_and_action(page, 'div.dashboard-action span.db-action-link', has_text="Action", description="Action Button")
-    locate_and_action(page, '#reportContainer div.actionModal li.action-modal-item span', has_text="Export", description="Export Option")
-    
-    time.sleep(2)
-    locate_and_action(page, 'div.selectAll label.check-label', action='Check', description="SelectAll box")
-    time.sleep(2)
-    locate_and_action(page, 'div.iterate-select select', action='Select', option='1: Object', description="Time Option")
-    locate_and_action(page, 'div.selectAll label.check-label', action='Check', description="SelectAll box")
-    time.sleep(2)
-    locate_and_action(page, 'div.fileType label.check-label span', has_text="Excel Spreadsheet", action="Check", description="Excel file type")
-    time.sleep(2)
-    locate_and_action(page, 'ul li label.check-label span', has_text="Pivot Table", action="Check", description="Pivot Table")
-    time.sleep(2)
-    locate_and_action(page, 'div.modal-footer div.exp-footer-button button', has_text="Export", description="Export button")
-    time.sleep(3)
-    locate_and_action(page, 'div.modal-dialog div.modal-content div button', has_text="Okay", description="Okay button")
-    time.sleep(3)
-    
+    export_flat_file(page, "Flat File - CD")
+    export_flat_file(page, "Flat File - NWNI")
+    export_flat_file(page, "Flat File - PSNI")
+    export_flat_file(page, "Flat File - PSNI")
+    export_flat_file(page, "Flat File - FSSI")
+    export_flat_file(page, "Flat File - Petrol CDNISI")
   except Exception as e:
     print("Error during download automation:", e)
+    
+    
+def export_flat_file(page, file_name):
+  print(f"\n--- Exporting {file_name} ---")
+  # Switch to file tab
+  locate_and_action(page, 'ul#report-nav-scroll li.reportNavLi', has_text=file_name, description=f"{file_name} tab")
+  
+  # Click Action button and choose export
+  locate_and_action(page, 'div.dashboard-action span.db-action-link', has_text="Action", description="Action Button")
+  locate_and_action(page, '#reportContainer div.actionModal li.action-modal-item span', has_text="Export", description="Export Option")
+
+  # Check SelectAll Box from both Geography and Time
+  locate_and_action(page, 'div.selectAll label.check-label', action='Check', description="SelectAll box")
+  time.sleep(2)
+  locate_and_action(page, 'div.iterate-select select', action='Select', option='1: Object', description="Time Option")
+  time.sleep(2)
+  locate_and_action(page, 'div.selectAll label.check-label', action='Check', description="SelectAll box")
+  time.sleep(2)
+  
+  # Select Excel and Choose Pivot Table
+  locate_and_action(page, 'div.fileType label.check-label span', has_text="Excel Spreadsheet", action="Check", description="Excel file type")
+  locate_and_action(page, 'ul li label.check-label span', has_text="Pivot Table", action="Check", description="Pivot Table")
+  time.sleep(3)
+  
+  # Click Export
+  locate_and_action(page, 'div.modal-footer div.exp-footer-button button', has_text="Export", description="Export button")
+  time.sleep(3) 
+  
+  # Close Information modal
+  locate_and_action(page, 'div.modal-dialog div.modal-content div button', has_text="Okay", description="Okay button")
+  time.sleep(5) 
 
 def locate_and_action(page, selector, has_text=None, action="Click", option=None, description="", timeout=10000):
   try:
